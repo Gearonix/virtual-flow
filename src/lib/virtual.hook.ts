14a90e6bf53975b18e39588b37816998403b2f87
Gearonix <egor.uzhanin@gmail.com>
2023-10-18 14:05:36 +0300

    refactored: incapsulated logic in virtual.hook.ts

---
+++ src/lib/virtual.hook.ts
@@ -0,0 +1,97 @@
+import { Nullable }        from '@grnx-utils/types'
+import { Undefinable }     from '@grnx-utils/types'
+import { useMemo }         from 'react'
+import { useLayoutEffect } from 'react'
+import { useState }        from 'react'
+
+export interface UseVirtualProps {
+  count: number
+  getScrollElement: () => Nullable<HTMLElement>
+  containerHeight: number
+  itemHeight: number
+  overscan?: number
+  scrollingDelay?: number
+}
+
+export interface VirtualItem {
+  idx: number
+  offsetTop: number
+}
+
+export const useVirtual = ({
+  count: itemsCount,
+  getScrollElement,
+  scrollingDelay = 100,
+  itemHeight,
+  containerHeight,
+  overscan = 3
+}: UseVirtualProps) => {
+  const [listHeight, setListHeight] = useState(0)
+  const [scrollTop, setScrollTop] = useState(0)
+  const [isScrolling, setIsScrolling] = useState(false)
+
+  useLayoutEffect(() => {
+    const scrollElement = getScrollElement()
+
+    if (!scrollElement) {
+      return
+    }
+
+    let timeoutId: Undefinable<NodeJS.Timeout>
+
+    const handleScroll = () => {
+      const scrollTop = scrollElement.scrollTop
+
+      setScrollTop(scrollTop)
+      setIsScrolling(true)
+
+      clearTimeout(timeoutId)
+
+      timeoutId = setTimeout(() => {
+        setIsScrolling(false)
+      }, scrollingDelay)
+    }
+
+    const resizeObserver = new ResizeObserver(([entry]) => {})
+
+    resizeObserver.observe(scrollElement)
+
+    scrollElement.addEventListener('scroll', handleScroll)
+
+    return () => {
+      resizeObserver.unobserve(scrollElement)
+      clearTimeout(timeoutId)
+      scrollElement.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  const virtualItems = useMemo(() => {
+    const rangeStart = scrollTop
+    const rangeEnd = scrollTop + containerHeight
+
+    let startIdx = Math.floor(rangeStart / itemHeight)
+    let endIdx = Math.ceil(rangeEnd / itemHeight)
+
+    startIdx = Math.max(0, startIdx - overscan)
+    endIdx = Math.min(itemsCount - 1, endIdx + overscan)
+
+    const virtualItems: VirtualItem[] = []
+
+    for (let i = startIdx; i <= endIdx; i++) {
+      virtualItems.push({
+        idx: i,
+        offsetTop: i * itemHeight
+      })
+    }
+
+    return virtualItems
+  }, [scrollTop, itemsCount])
+
+  const totalListHeight = itemHeight * itemsCount
+
+  return {
+    totalListHeight,
+    virtualItems,
+    isScrolling
+  }
+}
--- src/virtual.tsx
+++ src/virtual.tsx
@@ -1,98 +1,27 @@
-import { Undefinable }     from '@grnx-utils/types'
-import { useEffect }       from 'react'
-import { useLayoutEffect } from 'react'
-import { useMemo }         from 'react'
-import { useRef }          from 'react'
-import { useState }        from 'react'
+import { useCallback }      from 'react'
+import { useRef }           from 'react'
+import { useState }         from 'react'
+
+import { useVirtual }       from '@/lib/virtual.hook'
 
 const items = Array.from({ length: 1_000 }, (_, idx) => ({
   id: Math.random().toString(36).slice(2),
   text: String(idx)
 }))
 
-const ITEM_HEIGHT = 40
-const CONTAINER_HEIGHT = 610
-const OVERSCAN = 3
-const SCROLLING_DELAY = 100
-
 export const Virtual = () => {
   const [listItems, setListItems] = useState(items)
-  const [scrollTop, setScrollTop] = useState(0)
-  const [isScrolling, setIsScrolling] = useState(false)
 
   const scrollRef = useRef<HTMLDivElement>(null)
 
-  useEffect(() => {
-    const scrollElement = scrollRef.current
+  const virtualFlow = useVirtual({
+    count: listItems.length,
+    itemHeight: 40,
+    containerHeight: 610,
+    getScrollElement: useCallback(() => scrollRef.current, [])
+  })
 
-    if (!scrollElement) {
-      return
-    }
-
-    let timeoutId: Undefinable<NodeJS.Timeout>
-
-    const handleScroll = () => {
-      setIsScrolling(true)
-
-      clearTimeout(timeoutId)
-
-      timeoutId = setTimeout(() => {
-        setIsScrolling(false)
-      }, SCROLLING_DELAY)
-    }
-
-    scrollElement.addEventListener('scroll', handleScroll)
-
-    return () => {
-      clearTimeout(timeoutId)
-
-      scrollElement.removeEventListener('scroll', handleScroll)
-    }
-  }, [])
-
-  useLayoutEffect(() => {
-    const scrollElement = scrollRef.current
-
-    if (!scrollElement) {
-      return
-    }
-
-    const handleScroll = () => {
-      const scrollTop = scrollElement.scrollTop
-
-      setScrollTop(scrollTop)
-    }
-
-    scrollElement.addEventListener('scroll', handleScroll)
-
-    return () => {
-      scrollElement.removeEventListener('scroll', handleScroll)
-    }
-  }, [])
-
-  const virtualItems = useMemo(() => {
-    const rangeStart = scrollTop
-    const rangeEnd = scrollTop + CONTAINER_HEIGHT
-
-    let startIdx = Math.floor(rangeStart / ITEM_HEIGHT)
-    let endIdx = Math.ceil(rangeEnd / ITEM_HEIGHT)
-
-    startIdx = Math.max(0, startIdx - OVERSCAN)
-    endIdx = Math.min(listItems.length - 1, endIdx + OVERSCAN)
-
-    const virtualItems: any[] = []
-
-    for (let i = startIdx; i <= endIdx; i++) {
-      virtualItems.push({
-        idx: i,
-        offsetTop: i * ITEM_HEIGHT
-      })
-    }
-
-    return virtualItems
-  }, [scrollTop, listItems.length])
-
-  const totalListHeight = ITEM_HEIGHT * listItems.length
+  console.log(virtualFlow.isScrolling)
 
   return (
     <div>
@@ -108,22 +37,22 @@ export const Virtual = () => {
       <div
         ref={scrollRef}
         style={{
-          height: CONTAINER_HEIGHT,
+          height: 610,
           overflow: 'auto',
           border: '1px solid lightgrey',
           position: 'relative'
         }}>
         <div
           style={{
-            minHeight: totalListHeight
+            minHeight: virtualFlow.totalListHeight
           }}>
-          {virtualItems.map((virtualItem) => {
+          {virtualFlow.virtualItems.map((virtualItem) => {
             const item = listItems[virtualItem.idx]
             return (
               <div
                 key={virtualItem.idx}
                 style={{
-                  height: ITEM_HEIGHT,
+                  height: 40,
                   padding: '6px 12px',
                   border: '1px solid red',
                   boxSizing: 'border-box',
